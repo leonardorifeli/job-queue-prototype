220387f5bf8597161a8a635e1f2d7d7b2641ba69
Leonardo Rifeli <leonardorifeli@gmail.com>
2016-09-07 22:20:54 -0300

    Working with receive in job queue

--- core/src/main/java/com/mercury/model/Receive.java
+++ core/src/main/java/com/mercury/model/Receive.java
@@ -1,7 +1,31 @@
 package com.mercury.model;
 
-/**
- * Created by leonardo on 07/09/16.
- */
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+
+import java.io.IOException;
+import java.lang.InterruptedException;
+import java.util.concurrent.TimeoutException;
+
 public class Receive {
+
+    private final static String QUEUE_NAME = "hermes";
+
+    public static void main(String[] argv)
+            throws IOException,
+            InterruptedException,
+            TimeoutException {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+    }
+
 }
