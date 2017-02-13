9a28d7d177f4338166c763ac4ad7095e3f7b5c14
Leonardo Rifeli <leonardorifeli@gmail.com>
2017-02-12 23:23:54 -0200

    Replace space to tabs

--- core/src/main/java/com/mercury/business/enums/RabbitInfoToConfiguration.java
+++ core/src/main/java/com/mercury/business/enums/RabbitInfoToConfiguration.java
@@ -3,24 +3,24 @@ package com.mercury.business.enums;
 public abstract class RabbitInfoToConfiguration {
 
 	private final static String SERVER = "172.17.0.2";
-    private final static String USERNAME = "guest";
-    private final static String PASSWORD = "guest";
-    private final static Integer PORT = 5672;
+	private final static String USERNAME = "guest";
+	private final static String PASSWORD = "guest";
+	private final static Integer PORT = 5672;
 
-    public static String getServer() {
-    	return SERVER;
-    }
+	public static String getServer() {
+		return SERVER;
+	}
 
-    public static String getUsername() {
-    	return USERNAME;
-    }
+	public static String getUsername() {
+		return USERNAME;
+	}
 
-    public static String getPassword() {
-    	return PASSWORD;
-    }
+	public static String getPassword() {
+		return PASSWORD;
+	}
 
-    public static Integer getPort() {
-    	return PORT;
-    }
-    
+	public static Integer getPort() {
+		return PORT;
+	}
+	
 }
